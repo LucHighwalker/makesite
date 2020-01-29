d39823c973867c35852193dcde9c57919c084c24
LucHighwalker <luc.highwalker@gmail.com>
2020-01-28 16:57:52 -0800

    Parsing templates.

--- makesite.go
+++ makesite.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+type entry struct {
+	Name string
+	Done bool
+}
+
+type ToDo struct {
+	User string
+	List []entry
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	paths := []string{
+		"todo.tmpl",
+	}
+
+	todos := []entry{
+		entry{Name: "do this", Done: false},
+	}
+	user := ToDo{
+		User: "Me myself and I",
+		List: todos,
+	}
+
+	t := template.Must(template.New("todo.tmpl").ParseFiles(paths...))
+	err := t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(src string) string {
+	content, err := ioutil.ReadFile(src)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(content)
+}
+
+func writeFile(dst string, data []byte) error {
+	return ioutil.WriteFile(dst, data, 0644)
 }
